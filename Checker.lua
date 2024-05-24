7b1667aabda9c0ca83567b4f304e57e4c0e9b509
CrismonPetrasion <148696199+CrismonPetrasion@users.noreply.github.com>
2024-05-24 19:17:54 +0300

    Update Checker.lua

--- Checker.lua
+++ Checker.lua
@@ -1,7 +1,10 @@
 return {
+--// Buyed Until the Update with Days
 "C1059421-55D6-4E80-9A37-352B884BA1B0", -- @dr.rox, days: inf
 "D999B350-B524-4FB7-A7D6-C8C8F72CFCD8", -- @arteesso, days: inf
 "A388E830-3C87-4F5C-B550-9B3B88B003AB", -- @marix, days: inf
 "1A02E3D7-FA19-4498-8334-D31FFFADC9F0", -- @rreesse, days: inf
 "6DB0486F-B6B6-4ECB-B1C7-8AD9C8675C69", -- @cheese, days: inf
+--// Buyed When the Update with Days
+  
 }
