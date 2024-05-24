482104df93cfe26d7a35c822035bc15f23be9dfb
CrismonPetrasion <148696199+CrismonPetrasion@users.noreply.github.com>
2024-05-24 13:34:25 +0300

    Update Checker.lua

--- Checker.lua
+++ Checker.lua
@@ -1,4 +1,6 @@
 return {
 "C1059421-55D6-4E80-9A37-352B884BA1B0", -- @dr.rox, days: inf
 "D999B350-B524-4FB7-A7D6-C8C8F72CFCD8", -- @arteesso, days: inf
+"A388E830-3C87-4F5C-B550-9B3B88B003AB", -- @marix, days: inf
+"1A02E3D7-FA19-4498-8334-D31FFFADC9F0", -- @rreesse, days: inf
 }
