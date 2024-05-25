b0896291e6f1f9ebc6201103a733f62e27c2ebfe
CrismonPetrasion <148696199+CrismonPetrasion@users.noreply.github.com>
2024-05-25 13:33:55 +0300

    Update Checker.lua

--- Checker.lua
+++ Checker.lua
@@ -3,7 +3,6 @@ return {
 "C1059421-55D6-4E80-9A37-352B884BA1B0", -- @dr.rox, days: inf
 "A388E830-3C87-4F5C-B550-9B3B88B003AB", -- @marix, days: inf
 "1A02E3D7-FA19-4498-8334-D31FFFADC9F0", -- @rreesse, days: inf
-"6DB0486F-B6B6-4ECB-B1C7-8AD9C8675C69", -- @cheese, days: inf
 "63291CA0-EA96-425D-B094-812101C1D506", -- @child_executioner, days: inf
 --// Buyed When the Update with Days
 "8B2909D4-6901-4E40-A4BF-E3D411F4BAEC", -- @sigmahook, days: 
@@ -15,4 +14,5 @@ return {
 "D999B350-B524-4FB7-A7D6-C8C8F72CFCD8", -- @arteesso
 "78A2E3F9-7971-45DF-B926-FA7EA6565C83", -- @fizz
 "C1E96DA6-82D6-4927-8CE7-B5DDC8545422", -- @ntfofficial 
+"6DB0486F-B6B6-4ECB-B1C7-8AD9C8675C69", -- @cheese
 }
